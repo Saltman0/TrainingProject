683c3ebd8bd785e36b67927927dc89e87219eb8b
Saltman0 <baudoin.mathieu@gmail.com>
2024-09-23 19:05:36 +0200

    Pending...

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\BookingRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: BookingRepository::class)]
+#[ApiResource]
 class Booking
 {
     #[ORM\Id]
